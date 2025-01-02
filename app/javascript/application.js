48f1e10764db3d36dc7ee57cba6f990e3933a197
Leon Brimacombe <olibrimacombe@gmail.com>
2025-01-02 09:29:43 +0000

    amend section/content structure

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+var button = document.querySelector('#about-toggle');
+var background = document.querySelector('#background');
+
+button.addEventListener('click', () => {
+  element.classList.toggle("red");
+});
