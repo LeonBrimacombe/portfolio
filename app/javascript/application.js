96e4bec297e0dabe9aebecbfef2d482b356370fd
Leon Brimacombe <olibrimacombe@gmail.com>
2025-01-03 10:41:34 +0000

    show/hide JS for additional background div

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,24 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 // show-hide background copy
-const button = document.querySelector('#about-toggle');
-const element = document.querySelector('#background');
+const link = document.querySelector('.toggle');
+const show_link = document.getElementById('show-toggle');
+const hide_link = document.getElementById('hide-toggle');
 
-button.addEventListener('click', () => {
-  element.classList.toggle("hidden");
-});
+link.onclick = function() {
+  const div = document.getElementById('background');
+  toggleVisbility(div)
+  toggleVisbility(show_link)
+  toggleVisbility(hide_link)
+};
+
+function toggleVisbility(element) {
+  event.preventDefault();
+  if (element.style.display !== 'none') {
+    element.style.display = 'none';
+  }
+  else {
+      element.style.display = 'block';
+  };
+
+}
