baa8985e19ddb41a65f1490cadb5e578e5db8ef4
Leon Brimacombe <olibrimacombe@gmail.com>
2025-01-06 16:43:44 +0000

    correct show/hide link js functions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
 // show-hide background copy
-const link = document.querySelector('.toggle');
 const show_link = document.getElementById('show-toggle');
 const hide_link = document.getElementById('hide-toggle');
 
-link.onclick = function() {
+show_link.onclick = function() {
   const div = document.getElementById('background');
   toggleVisbility(div)
   toggleVisbility(show_link)
   toggleVisbility(hide_link)
 };
+
+hide_link.onclick = function() {
+  const div = document.getElementById('background');
+  toggleVisbility(div)
+  toggleVisbility(show_link)
+  toggleVisbility(hide_link)
+};
+
 
 function toggleVisbility(element) {
   event.preventDefault();
