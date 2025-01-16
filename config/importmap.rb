4eae465e0078dae38ac966964c09ae7c2f8d7e0f
Leon Brimacombe <olibrimacombe@gmail.com>
2025-01-16 11:32:22 +0000

    correcting importmap and application.js imports

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "components/show_hide", to: "/app/javascript/components/show_hide.js"
-pin "components/anchor_links", to: "/app/javascript/components/anchor_links.js"
-pin "components/sections_responses", to: "/app/javascript/components/section_responses.js"
+pin "components/show_hide", to: "components/show_hide.js"
+pin "components/anchor_links", to: "components/anchor_links.js"
+pin "components/section_responses", to: "components/section_responses.js"
